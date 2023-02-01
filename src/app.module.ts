42abe700d157f0c73cfb5b6b7d958e250a26157f
Mohand Amokrane Abdiche <m.amokrane.abdiche@gmail.com>
2023-02-01 15:36:06 +0100

    returning pollution data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirQualityModule } from './air-quality/air-quality.module';
 
 @Module({
-  imports: [],
+  imports: [AirQualityModule],
   controllers: [AppController],
   providers: [AppService],
 })
